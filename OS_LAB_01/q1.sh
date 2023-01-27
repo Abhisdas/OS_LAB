74a82880a1ac258995ab38dc5f269ef11e83c23b
Abhishek <dashabhish@gmail.com>
2023-01-28 00:02:02 +0530

    First Commit

--- OS_LAB_01/q1.sh
+++ OS_LAB_01/q1.sh
@@ -1,1 +1,15 @@
-echo "hello i am abhishek"
+<<Question
+Write a program to perform Addition, Subtraction, Multiplication, Division on two numbers inputed by user.
+Question
+
+echo "Enter Two Numbers Of Your Choice :-"
+read a b
+sum=$((a+b))
+sub=$((a-b))
+mul=$((a*b))
+div=$((a/b))
+echo "The sum of Numbers is: $sum"
+echo "The Subtraction of Numbers is: $sub"
+echo "The Multiplication of Numbers is: $mul"
+echo "The Division of Numbers is: $div"
+
